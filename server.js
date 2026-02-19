c63e1d4013d3a1fbae8b2688129fc65faed66da9
Ashutosh Goyal <ashu.toshgoyal1405@gmail.com>
2026-02-19 20:47:34 +0530

    DONEE 30% OF WORK

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
-const express = require('express');
-const app = express();
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const path = require("path");
 
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// 👇 IMPORTANT FIX
+app.use(express.static(path.join(__dirname, "Sanare", "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "Sanare", "public", "index.html"));
+});
+
+server.listen(3000, "0.0.0.0", () => {
+  console.log("Server running");
+});
 
